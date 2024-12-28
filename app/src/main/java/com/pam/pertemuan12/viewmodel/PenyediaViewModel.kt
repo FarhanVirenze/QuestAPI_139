f88fcedac641415475b8db88bc104b34a943f77d
farhan <farhanvirenze18@gmail.com>
2024-12-28 20:20:02 +0700

    membuat PenyediaViewModel.kt

---
+++ app/src/main/java/com/pam/pertemuan12/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.pam.pertemuan12.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.pam.pertemuan12.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
