1e23713185d9fb0f4f2ce2a61190cb5361522ae1
farhan <farhanvirenze18@gmail.com>
2024-12-28 20:17:17 +0700

    membuat Mahasiswa

---
+++ app/src/main/java/com/pam/pertemuan12/model/Mahasiswa.kt
@@ -0,0 +1,15 @@
+package com.pam.pertemuan12.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Mahasiswa(
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+    @SerialName(value = "jenis_kelamin")
+    val jeniskelamin: String,
+    val kelas: String,
+    val angkatan: String,
+)
