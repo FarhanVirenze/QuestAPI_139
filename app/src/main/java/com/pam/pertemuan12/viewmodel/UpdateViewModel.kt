c3bbb14101f12c88ff26ac6e473dda2d534f2d58
farhan <farhanvirenze18@gmail.com>
2024-12-29 14:59:31 +0700

    Membuat UpdateViewModel.kt.kt

---
+++ app/src/main/java/com/pam/pertemuan12/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,71 @@
+package com.pam.pertemuan12.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pam.pertemuan12.model.Mahasiswa
+import com.pam.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+
+    fun loadMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswaById(nim)
+                uiState = UpdateUiState(updateUiEvent = mahasiswa.toUpdateUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateMahasiswaState(updateUiEvent: UpdateUiEvent) {
+        uiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    suspend fun updateMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(nim, uiState.updateUiEvent.toMahasiswa())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class UpdateUiState(
+    val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
+)
+
+data class UpdateUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenis_kelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun UpdateUiEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenis_kelamin = jenis_kelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUpdateUiEvent(): UpdateUiEvent = UpdateUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenis_kelamin = jenis_kelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
