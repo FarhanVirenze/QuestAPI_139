d280058689d61c101ebce062d45bf81dd947a3b2
farhan <farhanvirenze18@gmail.com>
2024-12-29 14:58:19 +0700

    Fixed MahasiswaContainer.kt

--- app/src/main/java/com/pam/pertemuan12/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/pam/pertemuan12/dependenciesinjection/MahasiswaContainer.kt
@@ -1,6 +1,8 @@
-package com.pam.pertemuan12.repository
+package com.pam.pertemuan12.dependenciesinjection
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.pam.pertemuan12.repository.MahasiswaRepository
+import com.pam.pertemuan12.repository.NetworkMahasiswaRepository
 import com.pam.pertemuan12.service_api.MahasiswaService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -11,12 +13,15 @@ interface AppContainer {
 }
 
 class MahasiswaContainer : AppContainer {
-    private val baseUrl = "http://10.0.2.2:8000/umyTI/"
+    private val baseUrl = "http://10.0.2.2/umyTI/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl).build()
 
-    private val mahasiswaService: MahasiswaService by lazy { retrofit.create(MahasiswaService::class.java) }
-    override val mahasiswaRepository: MahasiswaRepository by lazy { NetworkMahasiswaRepository(mahasiswaService) }
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java) }
+
+    override val mahasiswaRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService) }
 }
